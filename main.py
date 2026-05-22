9f8cfb9d9929cba9abc81c431d6cfb7db26c6a80
weidai <david.g.adam@gmail.com>
2026-05-22 12:39:38 +0800

    complete fastapi full stach app with db config

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
-from fastapi import FastAPI, Request
+from typing import Annotated
+from fastapi import FastAPI, Request, status, HTTPException, Depends
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import JSONResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from starlette.exceptions import HTTPException as StarletteHTTPException
+from . import models
+from sqlmodel import or_, col, select, Session
+
+
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
+app.mount("/media", StaticFiles(directory="media"), name="media")
 templates = Jinja2Templates(directory="templates")
+
+models.create_db_and_tables()
 
 posts: list[dict] = [
     {
@@ -26,19 +37,188 @@ posts: list[dict] = [
 
 @app.get("/", include_in_schema=False, name="home")
 @app.get("/posts", include_in_schema=False, name="posts")
-def home(request: Request):
+def home(request: Request, db: Annotated[Session, Depends(models.get_db)]):
+    
+    posts = db.exec(select(models.Post)).all()
+
+
     return templates.TemplateResponse(request, 
         "home.html",
         {"posts": posts, "title": "Home Page"}
     )
 
 
-@app.get("/api/posts")
-def get_posts():
-    return {posts}
+@app.get("/posts/{post_id}", include_in_schema=False)
+def post_page(request: Request, post_id: int, db: Annotated[Session, Depends(models.get_db)]):
+    
+    post = db.exec(select(models.Post).where(models.Post.id == post_id)).first()
+
+    if post:
+        title = post.title[:50]
+        return templates.TemplateResponse(request,
+            "post.html", 
+            {"post": post, "title": title}
+        )
+
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Post not found")
+
+
+@app.get("/users/{user_id}/posts", include_in_schema=False, name="user_posts")
+def user_posts_page(
+    request: Request,
+    user_id: int,
+    db: Annotated[Session, Depends(models.get_db)],
+):
+    result = db.exec(select(models.User).where(models.User.id == user_id))
+    user = result.first()
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="User not found",
+        )
+    result = db.exec(select(models.Post).where(models.Post.user_id == user_id))
+    posts = result.all()
+    return templates.TemplateResponse(
+        request,
+        "user_posts.html",
+        {"posts": posts, "user": user, "title": f"{user.username}'s Posts"},
+    )
+
+
+@app.post(
+    "/api/users",
+    response_model=models.UserResponse,
+    status_code=status.HTTP_201_CREATED,
+)
+def create_user(user: models.UserCreate, db: Annotated[Session, Depends(models.get_db)]):
+    result = db.exec(
+        select(models.User).where(models.User.username == user.username),
+    )
+    existing_user = result.first()
+    if existing_user:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Username already exists",
+        )
+    result = db.exec(
+        select(models.User).where(models.User.email == user.email),
+    )
+    existing_email = result.first()
+    if existing_email:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Email already registered",
+        )
+    new_user = models.User(
+        username=user.username,
+        email=user.email,
+    )
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
+
+
+@app.get("/api/users/{user_id}", response_model=models.UserResponse)
+def get_user(user_id: int, db: Annotated[Session, Depends(models.get_db)]):
+    result = db.exec(
+        select(models.User).where(models.User.id == user_id),
+    )
+    user = result.first()
+    if user:
+        return user
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
+
+
+@app.get("/api/users/{user_id}/posts", response_model=list[models.PostResponse])
+def get_user_posts(user_id: int, db: Annotated[Session, Depends(models.get_db)]):
+    result = db.exec(select(models.User).where(models.User.id == user_id))
+    user = result.first()
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="User not found",
+        )
+    result = db.exec(select(models.Post).where(models.Post.user_id == user_id))
+    posts = result.all()
+    return posts
+
+
+@app.post("/api/posts", 
+    response_model=models.PostResponse,
+    status_code=status.HTTP_201_CREATED
+)
+def create_post(post: models.PostCreate, db: Annotated[Session, Depends(models.get_db)]):
+    result = db.exec(select(models.User).where(models.User.id == post.user_id))
+    user = result.first()
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="User not found",
+        )
+    new_post = models.Post(
+        title=post.title,
+        content=post.content,
+        user_id=post.user_id,
+    )
+    db.add(new_post)
+    db.commit()
+    db.refresh(new_post)
+    return new_post
 
 
 
+@app.get("/api/posts", response_model=list[models.PostResponse])
+def get_posts(db: Annotated[Session, Depends(models.get_db)]):
+    result = db.exec(select(models.Post))
+    posts = result.all()
+    return posts
 
-if __name__ == "__main__":
-    main()
+
+@app.get("/api/posts/{post_id}", response_model=models.PostResponse)
+def get_post(post_id: int, db: Annotated[Session, Depends(models.get_db)]):
+    result = db.exec(select(models.Post).where(models.Post.id == post_id))
+    post = result.first()
+    if post:
+        return post
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Post not found")
+
+
+@app.exception_handler(StarletteHTTPException)
+def general_http_exception_handler(request: Request, exc: StarletteHTTPException):
+    message = (
+        exc.detail if exc.detail 
+        else "An error occurred. Please check your request and try again."
+    )
+    
+    if request.url.path.startswith("/api"):
+        return JSONResponse(status_code=exc.status_code, content={"detail": message})
+    
+    return templates.TemplateResponse(
+        request,
+        "error.html", 
+        {
+            "status_code": exc.status_code,
+            "title": exc.status_code,
+            "message": message
+        },
+        status_code=exc.status_code
+    )
+
+@app.exception_handler(RequestValidationError)
+def validation_exception_handler(request: Request, exc: RequestValidationError):
+    if request.url.path.startswith("/api"):
+        return JSONResponse(
+            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+            content={"detail": exc.errors(), "body": exc.body},
+        )
+
+    return templates.TemplateResponse(request,"error.html", 
+        {
+            "status_code": status.HTTP_422_UNPROCESSABLE_ENTITY,
+            "title": status.HTTP_422_UNPROCESSABLE_CONTENT,
+            "message": "Invalid request. Please check your input and try again."
+        
+         },
+         status_code=status.HTTP_422_UNPROCESSABLE_ENTITY
+    )
---
+++ models.py
@@ -0,0 +1,81 @@
+from __future__ import annotations
+from datetime import UTC, datetime
+from mimetypes import init
+from typing import Annotated
+from pydantic import ConfigDict, EmailStr, Field
+from sqlalchemy.orm import Mapped, relationship
+from sqlmodel import Field, Session, SQLModel, create_engine, Relationship
+
+
+class UserBase(SQLModel):
+    username: Annotated[str, Field(min_length=1, max_length=50, unique=True, nullable=False)]
+    email: Annotated[EmailStr, Field(max_length=120, unique=True, nullable=False)]
+    image_file: Annotated[str | None, Field(max_length=200, nullable=True, default=None)]
+    
+    @property
+    def image_path(self) -> str:
+        if self.image_file:
+            return f"/media/profile_pics/{self.image_file}"
+        return "/media/profile_pics/default.jpg"
+
+class User(UserBase, table=True):
+    __tablename__ = "users"
+    id: Annotated[int | None, Field(default=None, primary_key=True, index=True)]
+    posts: Mapped[list["Post"]] = Relationship(
+        sa_relationship=relationship("Post", back_populates="author"))
+
+
+class UserCreate(UserBase):
+    pass
+
+
+class UserResponse(UserBase):
+    id: Annotated[int | None, Field(default=None, primary_key=True, index=True)]
+
+
+
+class PostBase(SQLModel):
+    title: Annotated[str, Field(min_length=1, max_length=100, index=True)]
+    content: Annotated[str, Field(min_length=1)]
+
+
+class Post(PostBase, table=True):
+    __tablename__ = "posts"
+    id: Annotated[int | None, Field(default=None, primary_key=True)]
+    date_posted: Annotated[datetime, Field(default=datetime.now(tz=UTC))]
+    user_id: Annotated[int, Field(foreign_key="users.id", nullable=False, index=True)]
+    author: Mapped[User | None] = Relationship(
+        sa_relationship=relationship("User", back_populates="posts"))
+
+class PostCreate(PostBase):
+    user_id: int
+
+
+class PostResponse(PostBase):
+    model_config = ConfigDict(from_attributes=True)
+    id: int
+    user_id: int
+    date_posted: datetime
+    author: UserResponse
+
+
+DATABASE_URL = "sqlite:///./blog.db"
+
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"check_same_thread": False}
+)
+
+engine.execution_options(autocommit=False, autoflush=False)
+
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+def get_db():
+    with Session(engine) as db:
+        yield db
+
+
+if __name__ == "__main__":
+    create_db_and_tables()
